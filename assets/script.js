fb7d45cdf51b60e07d89b6995424c1a436cdd931
Kaitlyn Giovinazzo <kaitlyn@pathwayscg.com>
2019-10-05 18:43:22 -0400

    adding some js

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    var gifs = ["cats", "cute animals", "dogs", "funny", "memes"];
+
+    function renderButtons() {
+        $("#gif-buttons").empty();
+        for (i = 0; i < gifs.length; i++) {
+            $("#gif-buttons").append("<button class = 'btn btn-success' data-gif='" + gifs[i] + "'>" + gifs[i] + "</button>");
+        }
+    }
+
+    renderButtons();
+
+    $("add-gif").on("click", function () {
+        event.preventDefault();
+        var gif = $("#gif-input").val().trim();
+        gifs.push(gif);
+        renderButtons();
+        return;
+    });
+
+    $("button").on("click", function () {
+        var gif = $(this).attr("data-gif");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            var results = response.data;
+            $("#gifs").empty();
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var gifImg = $("<img>");
+            }
+        })
+    })
